35965d6522008786e7e4417b48ece59cac8c5b62
Greg Hormann <ghormann@gmail.com>
2018-12-30 17:22:15 -0500

    Low priority queue

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ class MQTTClient:
         client.on_connect = on_connect
         client.on_message = on_message
         self.namequeue = []
+        self.namequeue_low = []
         client.message_callback_add("/christmas/personsName", self.on_name)
         client.message_callback_add("/christmas/personsNameFront", self.on_name_front)
         client.message_callback_add("/christmas/personsNameRemove", self.on_name_remove)
+        client.message_callback_add("/christmas/personsNameLow", self.on_name_low)
         client.message_callback_add("/christmas/currentSong", self.on_song)
         client.username_pw_set(config["username"], config["password"])
         client.connect(host=config["host"], port=config["port"])
@@ -29,6 +31,9 @@ class MQTTClient:
     def publishQueue(self):
         json_data = json.dumps(self.namequeue)
         self.client.publish("/christmas/nameQueue", json_data)
+
+        json_data = json.dumps(self.namequeue_low)
+        self.client.publish("/christmas/nameQueueLow", json_data)
 
     # The callback for Song 
     def on_song(self, client, userdata, msg):
@@ -52,8 +57,10 @@ class MQTTClient:
     def on_name_remove(self, client, userdata, msg):
         name = msg.payload.decode('UTF-8').upper()
         gen.logIt("Received Name for Remove " + msg.topic+" "+ name)
-        if name in self.namequeue:
+        while name in self.namequeue:
             self.namequeue.remove(name)
+        while name in self.namequeue_low:
+            self.namequeue_low.remove(name)
         self.publishQueue()
 
     # The callback for Names
@@ -67,10 +74,22 @@ class MQTTClient:
             gen.logIt('Adding to queue, size: ' + str(len(self.namequeue)))
         self.publishQueue() 
 
+    # The callback for Names Low Priority
+    def on_name_low(self, client, userdata, msg):
+        name = msg.payload.decode('UTF-8').upper()
+        gen.logIt("Received Low Priroity Name " + msg.topic+" "+ name)
+        if name in self.namequeue_low:
+            gen.logIt('Name already in Low queue')
+        else:
+            self.namequeue_low.append(name);
+            gen.logIt('Adding to Low queue, size: ' + str(len(self.namequeue)))
+        self.publishQueue() 
+
     def updateSong(self):
         gen.logIt("----------------------------")
         gen.logIt(datetime.datetime.now())
         gen.logIt('Queue size: ' + str(len(self.namequeue)))
+        gen.logIt('Queue size Low: ' + str(len(self.namequeue_low)))
         gen.logIt("----------------------------")
         use_me = []
         extra = baseNames.copy()
@@ -78,6 +97,8 @@ class MQTTClient:
         while len(use_me) < 13:
             if (len(self.namequeue) > 0):
                 use_me.append(self.namequeue.pop(0))
+            elif (len(self.namequeue_low) > 0):
+                use_me.append(self.namequeue_low.pop(0))
             else:
                 use_me.append(extra.pop(0))
         gen.logIt("Genereating with " + str(use_me))
@@ -96,6 +117,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("/christmas/personsName", 2)
     client.subscribe("/christmas/personsNameFront", 2)
     client.subscribe("/christmas/personsNameRemove", 2)
+    client.subscribe("/christmas/personsNameLow", 2)
     client.subscribe("/christmas/currentSong", 2)
 
     # Subscribing in on_connect() means that if we lose the connection and
